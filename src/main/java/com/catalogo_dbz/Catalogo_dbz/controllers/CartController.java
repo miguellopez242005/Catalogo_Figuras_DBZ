cdc2f6962fec7d365de3a20f44daa33ddac6db13
maicol00921 <maicolandresmarin903@gmail.com>
2026-04-09 14:30:10 -0500

    Merge branch 'Backend' of https://github.com/miguellopez242005/Catalogo_Figuras_DBZ into michael

--- src/main/java/com/catalogo_dbz/Catalogo_dbz/controllers/CartController.java
+++ src/main/java/com/catalogo_dbz/Catalogo_dbz/controllers/CartController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/cart")
+@RequestMapping("/cart")
 @RequiredArgsConstructor
 public class CartController {
 
